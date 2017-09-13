0a8d2d617053359cdf6a35cea1ebef73e8389c66
anytopkr <anytopkr@naver.com>
2017-09-14 00:03:56 +0900

    Update TeamProject.java

--- src/TeamProject.java
+++ src/TeamProject.java
@@ -6,6 +6,7 @@ public class TeamProject {
 		System.out.println("첫  git TeamProject 입니다");
 		System.out.println("두번째 추가해 봅니다.");
 		System.out.println("컴퓨터 공학과 20122968 이동혁 입니다. 잘 부탁드립니다.");
+		System.out.println("안녕하세요 황덕희 입니다.");
 	}
 
 }
