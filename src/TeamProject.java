49c8d6b5a65e6474461bad8545e82970f2a13ae7
zzo44 <zzo44@118.42.168.89>
2017-09-13 23:45:10 +0900

    8 line

--- src/TeamProject.java
+++ src/TeamProject.java
@@ -5,7 +5,7 @@ public class TeamProject {
 		// TODO Auto-generated method stub
 		System.out.println("첫  git TeamProject 입니다");
 		System.out.println("두번째 추가해 봅니다.");
-		System.out.println(" 컴퓨터 공학과 20122968 이동혁 입니다. 잘 부탁드립니다.");
+		System.out.println("컴퓨터 공학과 20122968 이동혁 입니다. 잘 부탁드립니다.");
 	}
 
 }
