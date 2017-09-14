65e352a515c57cc5d51110c5be154a54007d0b30
LCW <31894613+LcwHoseo@users.noreply.github.com>
2017-09-15 02:19:28 +0900

    Add files via upload

---
+++ src/Testjava_Lcw.java
@@ -0,0 +1,10 @@
+
+public class Testjava_Lcw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hellow ~ ");
+			System.out.println("My name is LCW");
+	}
+
+}
